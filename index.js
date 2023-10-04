db9c83b4294e125c57cc584c1d1b01056191be12
Matthew7991 <Matthew7991@gmx.de>
2023-10-04 13:17:13 +0200

    Kickoff

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import express from "express"
 import cors from "cors"
 import multer from "multer"
 import morgan from "morgan"
-import getDb from "./utilities/db"
-import { getRaids } from "./controller/raidController"
+import getDb from "./utilities/db.js"
+import { getRaids } from "./controller/raidController.js"
 
 const port = process.env.PORT
 
 const server = express()
-export let db = getDb()
+export const db = getDb()
 const upload = multer({ storage: multer.memoryStorage() })
 
 server.use(cors())
--- utilities/db.js
+++ utilities/db.js
@@ -1,13 +1,10 @@
 import { MongoClient } from "mongodb"
-import { db } from ".."
 
 const url = process.env.MONGODB_LOGIN
 const dbName = process.env.MONGODB_DBNAME
 const client = new MongoClient(url)
 
 export default async function getDb() {
-  if (db) return db
   await client.connect()
-  db = client.db(dbName)
-  return db
+  return client.db(dbName)
 }
